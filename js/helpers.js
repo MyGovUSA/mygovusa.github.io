fc684ea1a74ae5e78407632384f7a4915d599057
Ethan Naluz <e@ethannaluz.com>
2018-04-25 13:06:23 -0700

    Added navbar effects and scrap.html

--- js/helpers.js
+++ js/helpers.js
@@ -10,6 +10,7 @@ window.onscroll = () => {
     }
 };
 
+
 // Retrieved from w3schools, makes scrolling smooth on anchor tag clicks in navbar
 $(document).ready(function(){
     // Add smooth scrolling to all links
@@ -22,7 +23,9 @@ $(document).ready(function(){
             let hash = this.hash;
             // Using jQuery's animate() method to add smooth page scroll
             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({ scrollTop: $(hash).offset().top }, 800, function(){
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
                 // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
             });
